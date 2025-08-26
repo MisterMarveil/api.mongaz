2810c90502461dca3e040c1a65cf9bbee01d696a
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-08-26 18:54:34 +0100

    inserted JSONCONTAINS

--- src/Doctrine/JsonContainsFunction.php
+++ src/Doctrine/JsonContainsFunction.php
@@ -2,24 +2,23 @@
 namespace App\Doctrine;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
 class JsonContainsFunction extends FunctionNode
-{
+{    
     public $jsonField = null;
     public $value = null;
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-
+        //$parser->match(TokenType::T_IDENTIFIER); // (2)
+        $parser->match(TokenType::T_OPEN_PARENTHESIS); // (3)
         $this->jsonField = $parser->ArithmeticPrimary();
-        $parser->match(Lexer::T_COMMA);
-        $this->value = $parser->ArithmeticPrimary();
-
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_COMMA); // (5)
+        $this->value = $parser->ArithmeticPrimary(); // (6)
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);         
     }
 
     public function getSql(SqlWalker $sqlWalker): string
